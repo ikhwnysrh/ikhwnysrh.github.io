e80a9f653d451f36ce9f72602606c0467870756e
ikhwnysrh <ikhwanyusrah@gmail.com>
2023-10-03 21:11:32 +0800

    fitur dark mode

--- script.js
+++ script.js
@@ -7,8 +7,7 @@ window.onscroll = function () {
       document.body.scrollTop > 20 ||
       document.documentElement.scrollTop > 20
     ) {
-      document.getElementById("navbar").style.backgroundColor = "rgba(255, 255, 255, 0.7)";
-      document.getElementById("navbar").style.backdropFilter = "blur(3px)";
+      document.getElementById("navbar").style.backdropFilter = "blur(4px)";
       document.getElementById("navbar").style.marginTop = "0"; 
       document.getElementById("navbar").style.boxShadow = "0 1px 2px rgba(0, 0, 0, 0.2)";
       document.getElementById("button").classList.remove('visually-hidden');
@@ -19,3 +18,40 @@ window.onscroll = function () {
       document.getElementById("button").classList.add('visually-hidden');
     }
   }
+
+
+// Fungsi untuk mengaktifkan mode gelap
+function enableDarkMode() {
+  document.documentElement.setAttribute('data-bs-theme', 'dark');
+  localStorage.setItem('theme', 'dark');
+}
+
+// Fungsi untuk menonaktifkan mode gelap
+function disableDarkMode() {
+  document.documentElement.setAttribute('data-bs-theme', 'light');
+  localStorage.setItem('theme', 'light');
+}
+
+// Fungsi untuk memeriksa status mode gelap saat halaman dimuat
+function checkDarkMode() {
+  const savedTheme = localStorage.getItem('theme');
+  if (savedTheme === 'dark') {
+    enableDarkMode();
+  } else {
+    disableDarkMode();
+  }
+}
+
+// Tambahkan event listener untuk tombol toggle
+const darkModeToggle = document.getElementById('darkModeToggle');
+darkModeToggle.addEventListener('click', () => {
+  const currentTheme = document.documentElement.getAttribute('data-bs-theme');
+  if (currentTheme === 'dark') {
+    disableDarkMode();
+  } else {
+    enableDarkMode();
+  }
+});
+
+// Panggil fungsi untuk memeriksa status mode gelap saat halaman dimuat
+checkDarkMode();
